7277de90a563c6f31d12b5d794f191f1578e051d
Khánh Hoàng <hi@khanh.id>
2025-11-02 14:52:21 +0700

    Add `@types/bun` and `jsr.json` with version synchronization script
    - Add `@types/bun` dependency for improved type support in Bun projects.
    - Introduce `jsr.json` for project metadata and exports.
    - Create `tools/jsr.ts` script to synchronize the `jsr.json` version with `package.json`.

---
+++ tools/jsr.ts
@@ -0,0 +1,13 @@
+import path from 'node:path'
+import jsrJson from '../jsr.json'
+import packageJson from '../package.json'
+
+console.log(`NPM version: ${packageJson.version}`)
+console.log(`JSR version: ${jsrJson.version}`)
+
+await Bun.write(
+  path.resolve(__dirname, '../jsr.json'),
+  JSON.stringify({ ...jsrJson, version: packageJson.version }, null, 2) + '\n',
+)
+
+console.log('Updated jsr.json version to match package.json version.')
